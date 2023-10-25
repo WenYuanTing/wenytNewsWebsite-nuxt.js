5f3f36563158c4b5800076fe2b06ba6560a8ef1b
WenYT <mhome86@yahoo.com.tw>
2023-10-25 14:35:08 +0800

    update about page

--- pages/about.vue
+++ pages/about.vue
@@ -3,16 +3,24 @@
     <h1>關於</h1>
     <p>此網站透過Nuxt3+Vue3讀取外部新聞API製作而成。</p>
     <p>利用app.vue渲染整個頁面，包含header跟footer，</p>
-    <br />
+    <p>且用nuxt-link做到SPA</p>
+    <h2>首頁</h2>
+
     <p>
-      index.vue用useAsyncData()抓取到API資料後，將資料用v-for送到components資料夾中的New.vue將新聞製作出來。
+      用methods裡面的async function
+      useAsyncData()抓取到API資料後，將資料用v-for送到components資料夾中的New.vue將新聞製作出來。
     </p>
-    <br />
-    <P
-      >搜尋頁面使用v-model將input跟searchTerm綁定，一但有輸入文字就會觸發@input，透過handleInput()中的API去抓取搜尋相關的資料。
-    </P>
-    <p>再將抓取到的資料送給this.searchNewsData，就會透過v-for迴圈顯示出來</p>
-    <p>當input資料清空後，會延遲一下將this.searchNewsData清空</p>
+    <p>
+      且新增了兩個按鈕，透過v-show來決定是否要顯示新聞、顯示多久，再透過watch來顯示設定秒數的新聞
+    </p>
+
+    <h2>搜尋頁面</h2>
+    <p>為了避免對API的請求過快，在生命週期created的時候有先觸發debounce函式</p>
+    <p>
+      搜尋頁面使用v-model將input跟searchTerm綁定，輸入文字後的0.25秒就會觸發@input，透過debouncedHandleInput去抓取搜尋相關的資料。
+    </p>
+    <p>再將資料用v-for送到components資料夾中顯示出來</p>
+    <p>當input資料清空後，會延遲0.5秒將this.searchNewsData清空</p>
   </div>
 </template>
 
